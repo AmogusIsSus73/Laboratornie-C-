58a80fcce34c9cf6913f96993f37cf2ccae097f5
AmogusIsSus73 <116556479+AmogusIsSus73@users.noreply.github.com>
2024-02-12 21:09:31 +0400

    Add files via upload

---
+++ lab6(№2)/lab6(№2)/lab6(№2).cpp
@@ -0,0 +1,17 @@
+﻿//задание 2 ЛР 6 
+
+#include <stdio.h>
+
+void main() {
+    int i;
+    int stepen;
+
+    stepen = 1;
+    i = 0;
+
+    do {
+        printf("2^%d = %d\n", i, stepen);
+        i += 1;
+        stepen = stepen * 2;
+    } while (i <= 5);
+}
