7cfc6734b1ecb8d2877cd01724010bd75cb56180
AmogusIsSus73 <116556479+AmogusIsSus73@users.noreply.github.com>
2024-02-12 23:40:09 +0400

    Add files via upload

---
+++ lab8(1)/lab8(1)/lab8(1).cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+void aboutMe() {
+	printf("Шигапов\n");
+	printf("Данил\n");
+	printf("Растямович\n");
+	printf("\n");
+	printf("Группа: ИС-319\n");
+	printf("Дата: 15.01.2024\n");
+	printf("Лабораторная работа №1\n");
+	printf("\n");
+	printf("\n");
+	printf("          *          \n");
+	printf("       *     *       \n");
+	printf("          *          \n");
+	printf("   *             *   \n");
+	printf("          *          \n");
+	printf("          *          \n");
+	printf("*                   *\n");
+	printf("          *          \n");
+	printf("          *          \n");
+	printf("          *          \n");
+	printf("       * sus *       \n");
+	printf("     **       **     \n");
+	printf("   ***         ***   \n");
+	printf("   ___.........___   \n");
+	printf("  _(S) ( u )  ( S )_ \n");
+}
+
+void main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	aboutMe();
+}
---
+++ lab8(2)/lab8(2)/lab8(2).cpp
@@ -0,0 +1,318 @@
+﻿// lab8(2).cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "lab8(2).h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LAB82, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB82));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB82));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LAB82);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+void drawHome(HDC hdc) {
+    PAINTSTRUCT ps;
+    HPEN hPen;
+
+    // Крыша
+    hPen = CreatePen(PS_DASH, 5, RGB(255, 0, 0));
+    SelectObject(hdc, hPen);
+    MoveToEx(hdc, 150, 350, NULL);
+    LineTo(hdc, 275, 250);
+    LineTo(hdc, 400, 350);
+    // Дом
+    LineTo(hdc, 400, 525);
+    LineTo(hdc, 150, 525);
+    LineTo(hdc, 150, 350);
+    LineTo(hdc, 400, 350);
+    // Окно
+    MoveToEx(hdc, 225, 400, NULL);
+    LineTo(hdc, 225, 475);
+    LineTo(hdc, 325, 475);
+    LineTo(hdc, 325, 400);
+    LineTo(hdc, 225, 400);
+    // Рама
+    MoveToEx(hdc, 275, 400, NULL);
+    LineTo(hdc, 275, 475);
+    MoveToEx(hdc, 275, 425, NULL);
+    LineTo(hdc, 325, 425);
+
+    hPen = CreatePen(PS_DASH, 5, RGB(0, 255, 0));
+    SelectObject(hdc, hPen);
+    MoveToEx(hdc, 450, 525, NULL);
+    LineTo(hdc, 525, 450);
+    LineTo(hdc, 600, 525);
+    LineTo(hdc, 450, 525);
+
+    MoveToEx(hdc, 475, 450, NULL);
+    LineTo(hdc, 525, 400);
+    LineTo(hdc, 575, 450);
+    LineTo(hdc, 475, 450);
+
+    MoveToEx(hdc, 500, 400, NULL);
+    LineTo(hdc, 525, 375);
+    LineTo(hdc, 550, 400);
+    LineTo(hdc, 500, 400);
+
+    MoveToEx(hdc, 75, 525, NULL);
+    LineTo(hdc, 75, 325);
+
+    MoveToEx(hdc, 75, 515, NULL);
+    LineTo(hdc, 115, 485);
+
+    MoveToEx(hdc, 75, 500, NULL);
+    LineTo(hdc, 25, 450);
+
+    MoveToEx(hdc, 75, 475, NULL);
+    LineTo(hdc, 120, 440);
+
+    MoveToEx(hdc, 75, 460, NULL);
+    LineTo(hdc, 25, 415);
+
+    MoveToEx(hdc, 75, 440, NULL);
+    LineTo(hdc, 115, 400);
+
+    MoveToEx(hdc, 75, 420, NULL);
+    LineTo(hdc, 35, 375);
+
+    MoveToEx(hdc, 75, 400, NULL);
+    LineTo(hdc, 100, 375);
+
+    MoveToEx(hdc, 75, 375, NULL);
+    LineTo(hdc, 50, 350);
+}
+void drawCar(HDC hdc) {
+    HPEN hPen = CreatePen(PS_SOLID, 3, RGB(128, 0, 0));
+    SelectObject(hdc, hPen);
+
+    HBRUSH hBrush; //объект-кисть
+    hBrush = CreateSolidBrush(RGB(255, 128, 67)); //Сплошная кисть с цветом
+    SelectObject(hdc, hBrush);
+
+    Rectangle(hdc, 50, 200, 300, 250);
+
+    MoveToEx(hdc, 100, 200, NULL);
+    LineTo(hdc, 150, 150);
+    LineTo(hdc, 250, 150);
+    LineTo(hdc, 300, 200);
+
+    hPen = CreatePen(PS_SOLID, 7, RGB(0, 0, 128));
+    SelectObject(hdc, hPen);
+
+    hBrush = CreateHatchBrush(HS_FDIAGONAL, RGB(255, 0, 0));
+    SelectObject(hdc, hBrush);
+
+    Ellipse(hdc, 75, 225, 125, 275);
+
+    hPen = CreatePen(PS_SOLID, 7, RGB(0, 225, 0));
+    SelectObject(hdc, hPen);
+
+    hBrush = CreateHatchBrush(HS_FDIAGONAL, RGB(0, 128, 0));
+    SelectObject(hdc, hBrush);
+
+    Ellipse(hdc, 225, 225, 275, 275);
+}
+void drawTree(HDC hdc) {
+    
+    HPEN hPen = CreatePen(PS_SOLID, 3, RGB(0, 200, 0));
+    SelectObject(hdc, hPen);
+
+    int x1 = 0, y1 = 250; //кончик куста
+    int x2 = 100, y2 = 250; //корень куста
+    do {
+        //соединяем линией первую точку со второй
+        MoveToEx(hdc, x1, y1, NULL);
+        LineTo(hdc, x2, y2);
+        //первую точку перемещаем вправо на 25 пикселей
+        x1 += 5;
+        //перемещаем вверх на 50 пикселей
+        y1 -= 10;
+    } while (x1 <= 100);
+
+    x1 = 100;
+    y1 = 50;
+    do {
+        // соединяем линией первую точку со второй
+        MoveToEx(hdc, x1, y1, NULL);
+        LineTo(hdc, x2, y2);
+        // первую точку перемещаем вправо на 25 пикселей
+        x1 += 5;
+        // перемещаем вниз на 50 пикселей
+        y1 += 10;
+    } while (x1 <= 200);
+}
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+
+            drawHome(hdc); //из ЛР 5
+
+            drawCar(hdc);
+
+            drawTree(hdc);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
