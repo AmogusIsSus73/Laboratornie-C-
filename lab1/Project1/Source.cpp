2c067bc4506ed0ab3e6b39483e5e357154ea50a0
AmogusIsSus73 <116556479+AmogusIsSus73@users.noreply.github.com>
2024-02-12 20:35:03 +0400

    Add files via upload

---
+++ lab1/Project1/Source.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <Windows.h>
+
+
+void main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	printf("Шигапов\n");
+	printf("Данил\n");
+	printf("Растямович\n");
+	printf("\n");
+	printf("Группа: ИС-319\n");
+	printf("Дата: 15.01.2024\n");
+	printf("Лабораторная работа №1\n");
+	printf("\n");
+	printf("\n");
+	printf("       *          \n");
+	printf("    *     *       \n");
+	printf("       *          \n");
+	printf("*             *   \n");
+	printf("       *          \n");
+	printf("       *          \n");
+	printf("*                *\n");
+	printf("       *          \n");
+	printf("       *          \n");
+	printf("       *          \n");
+	printf("    * sus *       \n");
+	printf("  **       **     \n");
+	printf("***         ***   \n");
+	printf("___.........___   \n");
+	printf("_() ( A )  ( L )_ \n");
+}
